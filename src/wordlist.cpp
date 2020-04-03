3318974e71584c60ddc6f791d4a4aed6d984c8ee
Waqar Ahmed <waqar.17a@gmail.com>
2020-04-03 23:46:19 +0500

    wordlist class

--- src/wordlist.cpp
+++ src/wordlist.cpp
@@ -1,6 +1,70 @@
 #include "wordlist.h"
+#include <algorithm>
+#include <fstream>
 
-Wordlist::Wordlist()
+std::map<std::string, Wordlist> Wordlist::instances = {};
+
+Wordlist::Wordlist(const std::string& language) : m_language{language}, m_count{0}
 {
+    const std::string wordListFile = language + ".txt";
+    std::ifstream file(wordListFile);
+    std::string word;
+    while (std::getline(file, word)) {
+        m_words.emplace_back(word);
+        ++m_count;
+    }
 
+    if (m_count != 2048) {
+        throw WordlistException("BIP39 words list file must have precise 2048 entries");
+    }
+}
+
+Wordlist Wordlist::getLanguage(const std::string& language)
+{
+    auto it = instances.find(language);
+    if (it != instances.end()) {
+        return (*it).second;
+    }
+    auto wordList = Wordlist(language);
+    instances[language] = wordList;
+    return wordList;
+}
+
+Wordlist Wordlist::english()
+{
+    return getLanguage("english");
+}
+
+Wordlist Wordlist::french()
+{
+    return getLanguage("french");
+}
+
+Wordlist Wordlist::italian()
+{
+    return getLanguage("italian");
+}
+
+Wordlist Wordlist::spanish()
+{
+    return getLanguage("spanish");
+}
+
+std::string Wordlist::language() const
+{
+    return m_language;
+}
+
+std::string Wordlist::getWord(int index)
+{
+    if (index > (int)m_words.size())
+        return "";
+    return m_words.at(index);
+}
+
+int Wordlist::findIndex(const std::string& searchWord)
+{
+    auto pos = std::find(m_words.begin(), m_words.end(), searchWord);
+    int index = std::distance(m_words.begin(), pos);
+    return index;
 }
--- src/wordlist.h
+++ src/wordlist.h
@@ -1,11 +1,43 @@
 #ifndef WORDLIST_H
 #define WORDLIST_H
 
+#include <map>
+#include <string>
+#include <vector>
+
+class WordlistException
+{
+public:
+    WordlistException(std::string s) : m_desc{std::move(s)} {}
+    std::string what() const
+    {
+        return m_desc;
+    }
+
+private:
+    std::string m_desc;
+};
 
 class Wordlist
 {
-  public:
-    Wordlist();
+public:
+    Wordlist(const std::string& language);
+
+    static Wordlist getLanguage(const std::string& language);
+    static Wordlist english();
+    static Wordlist french();
+    static Wordlist italian();
+    static Wordlist spanish();
+
+    std::string language() const;
+    std::string getWord(int index);
+    int findIndex(const std::string& searchWord);
+
+private:
+    static std::map<std::string, Wordlist> instances;
+    std::vector<std::string> m_words;
+    std::string m_language;
+    int m_count;
 };
 
 #endif // WORDLIST_H
