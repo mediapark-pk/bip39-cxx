6bf61758a56e8e2be6391f8fd4c0ed7c5b3d2cd3
Waqar Ahmed <waqar.17a@gmail.com>
2020-04-05 03:11:09 +0500

    mnemonic class

--- src/mnemonic.cpp
+++ src/mnemonic.cpp
@@ -1,6 +1,87 @@
 #include "mnemonic.h"
+#include "pbkdf2_sha512/pbkdf2.hpp"
+#include "utils.h"
 
-Mnemonic::Mnemonic()
+#include <cstring>
+
+Mnemonic::Mnemonic(const std::string& entropy) : m_entropy{entropy}, m_wordsCount{0} {}
+
+std::string Mnemonic::generateSeed(const std::string& passphrase, int bytes)
 {
+    std::string pass = BIP39_Utils::Join(m_words, " ");
+    std::string salt = (std::string{"mnemonic"} + passphrase);
+    std::vector<uint8_t> output;
 
+    static constexpr int rounds = 2048;
+
+    pbkdf2_hmac_sha512(
+        reinterpret_cast<const uint8_t*>(pass.c_str()),
+        pass.length(),
+        reinterpret_cast<const uint8_t*>(salt.c_str()),
+        salt.length(),
+        rounds,
+        &output[0]);
+
+    std::string out{output.begin(), output.end()};
+    return out;
+}
+
+void Mnemonic::setWords(const std::vector<std::string>& words)
+{
+    m_words = words;
+}
+
+std::vector<std::string> Mnemonic::words() const
+{
+    return m_words;
+}
+
+std::vector<std::string> Mnemonic::rawBinaryChunks() const
+{
+    return m_rawBinaryChunks;
+}
+
+void Mnemonic::setRawBinaryChunks(const std::vector<std::string>& rawBinaryChunks)
+{
+    m_rawBinaryChunks = rawBinaryChunks;
+}
+
+std::vector<int> Mnemonic::wordsIndex() const
+{
+    return m_wordsIndex;
+}
+
+void Mnemonic::setWordsIndex(const std::vector<int>& wordsIndex)
+{
+    m_wordsIndex = wordsIndex;
+}
+
+void Mnemonic::appendWord(std::string word)
+{
+    m_words.emplace_back(std::move(word));
+}
+
+void Mnemonic::appendWordIndex(int wordIndex)
+{
+    m_wordsIndex.emplace_back(wordIndex);
+}
+
+void Mnemonic::appendBinaryChunk(std::string chunk)
+{
+    m_rawBinaryChunks.emplace_back(std::move(chunk));
+}
+
+void Mnemonic::incrementCount()
+{
+    ++m_wordsCount;
+}
+
+std::string Mnemonic::entropy() const
+{
+    return m_entropy;
+}
+
+void Mnemonic::setEntropy(const std::string &entropy)
+{
+    m_entropy = entropy;
 }
--- src/mnemonic.h
+++ src/mnemonic.h
@@ -1,11 +1,39 @@
 #ifndef MNEMONIC_H
 #define MNEMONIC_H
 
+#include <string>
+#include <vector>
 
 class Mnemonic
 {
-  public:
-    Mnemonic();
+public:
+    Mnemonic() = default;
+    Mnemonic(const std::string& entropy);
+    std::string generateSeed(const std::string& passphrase = "", int bytes = 0);
+
+    void setWords(const std::vector<std::string>& words);
+    std::vector<std::string> words() const;
+
+    std::vector<std::string> rawBinaryChunks() const;
+    void setRawBinaryChunks(const std::vector<std::string>& rawBinaryChunks);
+
+    std::vector<int> wordsIndex() const;
+    void setWordsIndex(const std::vector<int>& wordsIndex);
+
+    void appendWord(std::string word);
+    void appendWordIndex(int wordIndex);
+    void appendBinaryChunk(std::string chunk);
+    void incrementCount();
+
+    std::string entropy() const;
+    void setEntropy(const std::string& entropy);
+
+private:
+    std::string m_entropy;
+    int m_wordsCount;
+    std::vector<int> m_wordsIndex;
+    std::vector<std::string> m_words;
+    std::vector<std::string> m_rawBinaryChunks;
 };
 
 #endif // MNEMONIC_H
