d985291ad11f444a456887568941b07c71b23bc0
Waqar Ahmed <waqar.17a@gmail.com>
2020-04-06 10:33:53 +0500

    test vectors

--- main.cpp
+++ main.cpp
@@ -1,21 +1,42 @@
+#include <cassert>
 #include <iostream>
 
 #include "src/bip39.h"
 #include "src/mnemonic.h"
+#include "src/utils.h"
+
+static std::string joined_mnemonic(const std::vector<std::string>& s)
+{
+    return BIP39_Utils::Join(s, " ");
+}
 
 int main()
 {
-    //    auto mn = BIP39::Generate(12);
-    //    std::cout << "Entropy: " << mn.entropy() << std::endl;
+    std::string passPhrase = "TREZOR";
 
-    //    std::cout << "seed: " << mn.generateSeed("waqar") << std::endl;
     auto m = BIP39::Entropy("00000000000000000000000000000000");
-    std::cout << m.entropy() << std::endl;
-    for (auto a : m.words()) {
-        std::cout << a << " ";
-    }
-    std::cout << std::endl;
-    std::cout << m.generateSeed() << std::endl;
+    assert(m.entropy() == "00000000000000000000000000000000");
+    assert(
+        joined_mnemonic(m.words()) ==
+        "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon "
+        "about");
+    std::string exp =
+        "C55257C360C07C72029AEBC1B53C05ED0362ADA38EAD3E3E9EFA3708E53495531F09A6987599D18264C1E1C92F"
+        "2CF141630C7A3C4AB7C81B2F001698E7463B04";
+    std::string actual = BIP39_Utils::base16Encode(m.generateSeed("TREZOR"));
+    assert(actual == exp);
+
+
+    m = BIP39::Entropy("7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f");
+    assert(m.entropy() == "7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f");
+    assert(
+        joined_mnemonic(m.words()) ==
+        "legal winner thank year wave sausage worth useful legal winner thank yellow");
+    exp =
+        "2E8905819B8723FE2C1D161860E5EE1830318DBF49A83BD451CFB8440C28BD6FA457FE1296106559A3C80937A1"
+        "C1069BE3A3A5BD381EE6260E8D9739FCE1F607";
+    actual = BIP39_Utils::base16Encode(m.generateSeed("TREZOR"));
+    assert(actual == exp);
 
 
     return 0;
