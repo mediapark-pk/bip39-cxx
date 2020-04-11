27ea9802da9a002eb6b2aee559aea80fb97e940c
Waqar Ahmed <waqar.17a@gmail.com>
2020-04-11 22:07:23 +0500

    direct acess to members

--- src/mnemonic.cpp
+++ src/mnemonic.cpp
@@ -27,57 +27,12 @@ std::string Mnemonic::generateSeed(const std::string& passphrase, int bytes)
     return out;
 }
 
-void Mnemonic::setWords(const std::vector<std::string>& words)
-{
-    m_words = words;
-}
-
 std::vector<std::string> Mnemonic::words() const
 {
     return m_words;
 }
 
-std::vector<std::bitset<11>> Mnemonic::rawBinaryChunks() const
-{
-    return m_rawBinaryChunks;
-}
-
-std::vector<int> Mnemonic::wordsIndex() const
-{
-    return m_wordsIndex;
-}
-
-void Mnemonic::setWordsIndex(const std::vector<int>& wordsIndex)
-{
-    m_wordsIndex = wordsIndex;
-}
-
-void Mnemonic::appendWord(std::string word)
-{
-    m_words.emplace_back(std::move(word));
-}
-
-void Mnemonic::appendWordIndex(int wordIndex)
-{
-    m_wordsIndex.emplace_back(wordIndex);
-}
-
-void Mnemonic::appendBinaryChunk(std::bitset<11> chunk)
-{
-    m_rawBinaryChunks.emplace_back(std::move(chunk));
-}
-
-void Mnemonic::incrementCount()
-{
-    ++m_wordsCount;
-}
-
 std::string Mnemonic::entropy() const
 {
     return m_entropy;
-}
-
-void Mnemonic::setEntropy(const std::string &entropy)
-{
-    m_entropy = entropy;
 }
--- src/mnemonic.h
+++ src/mnemonic.h
@@ -25,29 +25,17 @@ public:
     Mnemonic(const std::string& entropy);
     std::string generateSeed(const std::string& passphrase = "", int bytes = 0);
 
-    void setWords(const std::vector<std::string>& words);
-    std::vector<std::string> words() const;
-
-    std::vector<std::bitset<11>> rawBinaryChunks() const;
-    void setRawBinaryChunks(const std::vector<std::string>& rawBinaryChunks);
-
-    std::vector<int> wordsIndex() const;
-    void setWordsIndex(const std::vector<int>& wordsIndex);
-
-    void appendWord(std::string word);
-    void appendWordIndex(int wordIndex);
-    void appendBinaryChunk(std::bitset<11> chunk);
-    void incrementCount();
-
-    std::string entropy() const;
-    void setEntropy(const std::string& entropy);
-
-private:
     std::string m_entropy;
     std::vector<int> m_wordsIndex;
     std::vector<std::string> m_words;
     std::vector<std::bitset<11>> m_rawBinaryChunks;
     int m_wordsCount;
+
+    std::string entropy() const;
+
+    std::vector<std::string> words() const;
+
+private:
     static constexpr int BIP39_SEED_LEN_512 = 64;
 };
 
